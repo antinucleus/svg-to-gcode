90cc446538a48385d060502e6e093ce87bfab827
antinucleus <antinucleuss@gmail.com>
2024-10-29 12:01:03 +0300

    feat: add husky, commitlint and lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ['expo', 'eslint:recommended'],
+  extends: ['expo', 'eslint:recommended', 'prettier'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
